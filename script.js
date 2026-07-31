f1813154a21be2fca229d1ccc195d2161a797cec
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-31 07:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
+        }
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+
     let result = [];
     let chunk = [];
     let sum = 0;
 
     for(let i = 0; i < arr.length; i++) {
 
-        if(sum + arr[i] <= maxSum) {
+        if(sum + arr[i] <= n) {
+
             chunk.push(arr[i]);
             sum += arr[i];
+
         } else {
+
             result.push(chunk);
             chunk = [arr[i]];
             sum = arr[i];
+
         }
 
     }
 
     result.push(chunk);
 
-    return divide;
-		
-		
-		
-		
+    return result;
+
 };
 
-const n = prompt("Enter n: ");
+const n = Number(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
