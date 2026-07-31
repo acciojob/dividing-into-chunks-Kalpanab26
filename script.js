dedbbc87515c592f537d8fd014b4078e47d487ec
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-31 08:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-        }
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 let arr = [4,3,2,1];
-let n = 3;
-let chunk =[];
+let n = 4;
+
+let chunk = [];
 let sum = 0;
 let result = [];
-for(let i =0;i<arr.length;i++){
-    if(sum + arr[i]<= n){
-        chunk.push(arr[i])
-        sum= sum + arr[i];
-    } else{
+
+for(let i = 0; i < arr.length; i++){
+
+    if(sum + arr[i] <= n){
+
+        chunk.push(arr[i]);
+        sum += arr[i];
+
+    } else {
+
         result.push(chunk);
+
         chunk = [arr[i]];
         sum = arr[i];
+
     }
-  
+
 }
+
 result.push(chunk);
 
 console.log(result);
