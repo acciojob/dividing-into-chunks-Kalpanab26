88b2c292d08872437ed90c25b404d659a4ac1313
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-31 07:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
         }
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-const divide = (arr, n) => {
-
-    let result = [];
-    let chunk = [];
-    let sum = 0;
-
-    for(let i = 0; i < arr.length; i++) {
-
-        if(sum + arr[i] <= n) {
-
-            chunk.push(arr[i]);
-            sum += arr[i];
-
-        } else {
-
-            result.push(chunk);
-            chunk = [arr[i]];
-            sum = arr[i];
-
-        }
-
+let arr = [4,3,2,1];
+let n = 3;
+let chunk =[];
+let sum = 0;
+let result = [];
+for(let i =0;i<arr.length;i++){
+    if(sum + arr[i]<= n){
+        chunk.push(arr[i])
+        sum= sum + arr[i];
+    } else{
+        result.push(chunk);
+        chunk = [arr[i]];
+        sum = arr[i];
     }
+  
+}
+result.push(chunk);
 
-    result.push(chunk);
-
-    return result;
-
-};
+console.log(result);
 
 const n = Number(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
